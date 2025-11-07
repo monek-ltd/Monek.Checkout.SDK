8f936a80529b89c1f1f425e766b3e9f2038c9abb
The Lighthouse <157377631+Humberstone83-X@users.noreply.github.com>
2025-11-07 11:42:11 +0000

    MON-2924/Add-early-Apple (#46)

--- SDK/src/sdk/types/callbacks.ts
+++ SDK/src/sdk/types/callbacks.ts
@@ -1,7 +1,9 @@
 import type { AmountInput, CardholderDetails } from './transaction-details';
+import type { ExpressPaymentDetails } from './express';
 
 export type InitCallbacks = {
     getAmount?: () => Promise<AmountInput> | AmountInput;
     getCardholderDetails?: () => Promise<CardholderDetails> | CardholderDetails;
     getDescription?: () => Promise<string> | string;
+    onExpressPaymentDetails?: (details: ExpressPaymentDetails) => Promise<void> | void;
 }
