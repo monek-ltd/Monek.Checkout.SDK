4d26f6fccc3de84cb2ccd7e5f5acb25de9246550
Klairi Tasopoulou <klairi.tasopoulou@monek.com>
2026-04-27 14:22:16 +0100

    MON-3243 .env overrides config

--- SDK/src/sdk/config.ts
+++ SDK/src/sdk/config.ts
@@ -1,11 +1,11 @@
 export const RUNTIME_ENV = 'prod';
 export const API = {
-            base: 'https://api.monek.com/embedded-checkout',
-            appleSession: 'https://api.monek.com/apple-pay/session',
-        };
+    base: import.meta.env.VITE__MONEK_API || 'https://api.monek.com/embedded-checkout',
+    appleSession: import.meta.env.VITE__MONEK_APPLE_SESSION || 'https://api.monek.com/apple-pay/session',
+};
 export const WS = {
-            base: 'wss://wqen1zbhll.execute-api.eu-west-2.amazonaws.com/v1/',
-        };
+    base: import.meta.env.VITE__MONEK_WS || 'wss://wqen1zbhll.execute-api.eu-west-2.amazonaws.com/v1/',
+};
 export const FRAMES = {
-                base: 'https://checkout-js.monek.com',
-            };
+    base: import.meta.env.VITE__MONEK_FRAMES || 'https://checkout-js.monek.com',
+};
